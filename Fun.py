48af55ae83cf3301532fd33600695a757fc39a58
Injamamul <36126059+injamamul-haque@users.noreply.github.com>
2020-07-02 08:04:52 +0530

    Add files via upload

---
+++ Fun.py
@@ -0,0 +1,91 @@
+import webbrowser
+import os
+import datetime
+import pyaudio
+import pyttsx3
+import wikipedia
+import speech_recognition as sr
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am jarvis  Your virtual friend   Sir. your  friend is ready to work with you ")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"Boss said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    loop=True
+    while loop:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=10)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play movie' in query:
+            movie_dir = 'G:\\movie'
+            movies = os.listdir(movie_dir)
+            print(movies)    
+            os.startfile(os.path.join(movie_dir, movies[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+		
+        elif 'open code' in query:
+            codePath = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"
+            os.startfile(codePath)
+        elif 'stop running' in query:
+		        loop=False
+		    
